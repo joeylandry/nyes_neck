9ebd9554bfefe8f7a19671adfe1184712daa9808
joeylandry <joseph.landry@tufts.edu>
2026-07-01 14:27:50 -0400

    Configure Sanity Studio deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,4 +3,5 @@ import { sanityDataset, sanityProjectId } from "./src/sanity/env";
 
 export default defineCliConfig({
   api: { projectId: sanityProjectId ?? "missing", dataset: sanityDataset },
+  deployment: { appId: "xtx1pc3qrv8zeksq3ny2qqhp" },
 });

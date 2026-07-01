2dfb63d710acfb09f39dedaa743fce3fcb4736cf
joeylandry <joseph.landry@tufts.edu>
2026-07-01 15:37:56 -0400

    Fix Sanity Studio project configuration

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,11 @@
-export const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const sanityDataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production";
+export const sanityProjectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ??
+  process.env.SANITY_STUDIO_PROJECT_ID ??
+  "stfnq920";
+export const sanityDataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET ??
+  process.env.SANITY_STUDIO_DATASET ??
+  "production";
 export const sanityApiVersion = "2026-07-01";
 
 export const isSanityConfigured = Boolean(sanityProjectId && sanityDataset);
